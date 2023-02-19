5141052054f733b925428a5039de304913a6652f
Gilmar Silva <gilmar.filho@contaazul.com>
2023-02-19 17:34:07 -0300

    chore: refactor

--- src/cluster/cluster.service.ts
+++ src/cluster/cluster.service.ts
@@ -1,38 +1,43 @@
-import cluster from "cluster";
-import * as process from "process";
-import * as os from "os";
-import {Logger} from "@nestjs/common";
+import cluster, { Worker } from 'cluster';
+import os from 'os';
+import { Logger } from '@nestjs/common';
 
 export class AppClusterService {
-    static clusterize(workers: Number, callback: Function): void {
+    static clusterize(workers: number, callback: () => void): void {
         if (cluster.isPrimary) {
+            const numCPUs = os.cpus().length;
+            const numWorkers = Math.min(numCPUs, workers);
 
             Logger.log(`Primary server started on ${process.pid}`);
 
-            const processesCount = os.cpus().length
-
-            for (let index = 0; index < processesCount; index++) {
-                cluster.fork()
+            for (let i = 0; i < numWorkers; i++) {
+                cluster.fork();
             }
 
-            // process.on('SIGINT', function () {
-            //     console.log('Cluster shutting down...');
-            //     for (var id in cluster.workers) {
-            //         cluster.workers[id].kill();
-            //     }
-            //     // exit the master process
-            //     process.exit(0);
-            // });
-            //
-            //
-            cluster.on('online', function (worker) {
-                Logger.log( 'Worker ' + worker.process.pid + ' is online.' );
+            cluster.on('online', (worker: Worker) => {
+                Logger.log(`Worker ${worker.process.pid} is online.`);
             });
 
-            cluster.on('exit', (worker, code, signal) => {
-                Logger.log(`Worker ${worker.process.pid} died. Restarting`);
-                cluster.fork();
-            })
+            // cluster.on('exit', (worker: Worker, code: number, signal: string) => {
+            //     Logger.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}. Restarting.`);
+            //     cluster.fork();
+            // });
+            process.on( "SIGTERM", function() {
+                console.log( "\ngracefully shutting down from SIGINT (Crtl-C)" );
+                process.exit();
+            } );
+
+            process.once('SIGTERM', () => {
+                console.log("oii")
+                Logger.log('Cluster shutting down...');
+                for (const id in cluster.workers) {
+                    if (cluster.workers.hasOwnProperty(id)) {
+                        const worker = cluster.workers[id];
+                        worker.kill();
+                    }
+                }
+                process.exit(0);
+            });
         } else {
             callback();
         }
